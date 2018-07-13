e0240df287a3b561b31249a6210c05afd107e113
Philipp Dressel <philipp@dressel.io>
2018-07-13 15:01:15 +0200

    Changed chatty to send large respones instead of log spam

--- chatty/chatty.py
+++ chatty/chatty.py
@@ -1,19 +1,21 @@
 from flask import Flask
-import time
-import threading
+import os
+import sys
 
 app = Flask(__name__)
 
+def important_information(n):
+    min_lc = ord(b'a')
+    len_lc = 26
+    ba = bytearray(os.urandom(n))
+    for i, b in enumerate(ba):
+        ba[i] = min_lc + b % len_lc # convert 0..255 to 97..122
+    return ba.decode("utf-8")
+
 @app.route('/')
 def root():
-    return 'I like to be very verbose with my logs 😇'
+    return 'I like to be very verbose 😇<br/>Here is some stuff you absolutely need to know:<br/>' + important_information(2000000)
 
 @app.route('/health')
 def health():
     return ''
-
-def spam():
-    print('✅ alert: everything is okay')
-    threading.Timer(0.5, spam).start()
-
-spam()
